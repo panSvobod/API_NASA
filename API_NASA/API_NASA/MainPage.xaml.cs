aa0004e871b3a2e98c228ab3d624d46929b7d5b5
Kila5 <Kila5@KRISTIAN>
2023-12-17 14:17:59 +0100

    Přidejte soubory projektu.

---
+++ API_NASA/API_NASA/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace API_NASA
+{
+    public partial class MainPage : ContentPage
+    {
+        private Random gen = new Random();
+        DateTime RandomDay()
+        {
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(gen.Next(range));
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
